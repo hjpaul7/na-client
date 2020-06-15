84df7c240d2cac14d7a8bf4ddfb78b197375ea85
hustin-paul <hj.paul7@gmail.com>
2020-06-15 01:15:11 -0400

    fixes

--- react-fundamentals/src/components/concepts/RehabCenters.js
+++ react-fundamentals/src/components/concepts/RehabCenters.js
@@ -20,25 +20,33 @@ const RehabCenters = () => {
       <h4>Rehab Centers in Indianapolis</h4>
       <hr />
       <h6>
-        <a href="/">Salvation Army Harbor Lights Center</a>
+        <a href="https://centralusa.salvationarmy.org/harborlightindiana/">
+          Salvation Army Harbor Lights Center
+        </a>
       </h6>
       <li>2400 N. Tibbs Ave</li>
       <li>(317) 972-1450</li>
       <hr />
       <h6>
-        <a href="/">Fairbanks Addiction Treatment and Recovery Center</a>
+        <a href="https://www.fairbankscd.org/">
+          Fairbanks Addiction Treatment and Recovery Center
+        </a>
       </h6>
       <li>8102 Clearvista Parkway</li>
       <li>(317) 849-8222</li>
       <hr />
       <h6>
-        <a href="/">Roudebush VA Substance Abuse Treatment Section</a>
+        <a href="https://www.indianapolis.va.gov/services/Mental_Health_Services.asp">
+          Roudebush VA Substance Abuse Treatment Section
+        </a>
       </h6>
       <li>1481 W. 10th St</li>
       <li>(317) 554-0000</li>
       <hr />
       <h6>
-        <a href="/">Life Recovery Center East</a>
+        <a href="https://www.liferecoverycenterindy.com/">
+          Life Recovery Center East
+        </a>
       </h6>
       <li>4455 McCoy St</li>
       <li>(317) 887-3290</li>
@@ -50,13 +58,15 @@ const RehabCenters = () => {
       <li>(317) 525-0226</li>
       <hr />
       <h6>
-        <a href="/">Community Hospital DBA</a>
+        <a href="https://yourcommunityhospital.com">Community Hospital DBA</a>
       </h6>
       <li>6950 Hillsdale Court</li>
       <li>(317) 621-7600</li>
       <hr />
       <h6>
-        <a href="/">Indianapolis Treatment Center</a>
+        <a href="https://www.indianactc.com/location/indianapolis/">
+          Indianapolis Treatment Center
+        </a>
       </h6>
       <li>2626 E. 46th St</li>
       <li>(855) 509-8002</li>
