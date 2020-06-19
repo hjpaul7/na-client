dfe5e4be8074f5e2416431371faa4faf5d17011f
hustin-paul <hj.paul7@gmail.com>
2020-06-19 12:59:45 -0400

    fixes

--- react-fundamentals/src/components/concepts/AddictiveDrugs.js
+++ react-fundamentals/src/components/concepts/AddictiveDrugs.js
@@ -1,7 +1,26 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
 const AddictiveDrugs = () => {
+  const classes = useStyles();
+
   const Row = styled.div`
     display: flex;
     flex-direction: row;
